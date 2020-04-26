6bf91429a7677bbecd0581239a2f68e682dbe0cc
mario piccinelli <mario.piccinelli@gmail.com>
2020-04-26 22:20:16 +0200

    reimpostati stili home (con withstyles) e header (con componenti material)

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Viewer from './Viewer';
 import FilesTree from './FilesTree';
-import { Button } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Drawer from '@material-ui/core/Drawer';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import path from 'path'
 import fs from 'fs'
-import os from 'os'
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 // Modifiche per funzionamento worker react-pdf
 import { pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `http://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const drawerWidth = 240;
+// Costruzione stili per home -------------------------------------------------
+
+const headerHeight = 45;
+const menuWidth = 250;
 
 const styles = (theme) => ({
 		
@@ -25,49 +26,45 @@ const styles = (theme) => ({
 		display: 'flex',
 	},
 
-	appBar: {
-		zIndex: theme.zIndex.drawer + 1,
+	headerDiv: {
+		position: "absolute",
+		height: headerHeight,
+		left: 0,
+		right: 0,
+		top: 0,
+		display: "flex",
+		alignItems: "center",
+		backgroundColor: "#4ab1b9"
 	},
 	
-	drawer: {
-		width: drawerWidth,
-		flexShrink: 0,
-	},
-	
-	drawerPaper: {
-		width: drawerWidth,
-	},
+	treeDiv: {
+		position: "absolute",
+		top: headerHeight,
+		left: 0,
+		width: menuWidth,
+		bottom: 0,
 
-	drawerContainer: {
-		overflow: 'auto',
-	},	
+		overflowX: "hidden",
+		overflowY: "auto",
+
+		borderRight: "1px solid darkgray"
+	},
 	
-	// necessary for content to be below app bar
-	toolbar: theme.mixins.toolbar,
-	content: {
-		flexGrow: 1,
-		backgroundColor: theme.palette.background.default,
-		padding: theme.spacing(3),
+	viewerDiv: {
+		position: "absolute",
+		top: headerHeight,
+		left: menuWidth+5,
+		right: 0,
+		bottom: 0,
+
+		//overflowX: "auto",
+		//overflowY: "auto"	
 	},
 
 });
 
+// ----------------------------------------------------------------------------
 class App extends React.Component {
-
-		/*
-        const { remote } = window.require('electron')
-        console.log("appData", remote.app.getPath('appData'))
-        console.log("userData", remote.app.getPath('userData'))
-		console.log("appPath", remote.app.getAppPath())
-		*/
-
-		/*
-        fs.readdir(this.state.fileFolder, (err, dir) => {
-            for (let filePath of dir) {
-                console.log(filePath);
-            }
-		});
-		*/
 
 	static propTypes = {
 		classes: PropTypes.object.isRequired,
@@ -158,54 +155,27 @@ class App extends React.Component {
 
 	render(){
 
-		const headerHeight = 50;
-		const menuWidth = 250;
+		// Classi per gli stili custom
+		const {classes} = this.props;
 
 		return (
 
 			<React.Fragment>
 
-			<div
-				style={{
-					position: "absolute",
-					height: headerHeight,
-					left: 0,
-					right: 0,
-					top: 0,
+			<CssBaseline />
 
-					backgroundColor: "lightGray",
-
-					display: "flex",
-					alignItems: "center"
-
-				}}
-			>
-				<span
-					style={{
-						fontSize: "x-large",
-						fontWeight: "bold"
-					}}
-				>
-					Charts Viewer
-				</span>
+			<div className={classes.headerDiv}>
+				<Toolbar variant="dense">
+					<IconButton edge="start" color="inherit" aria-label="menu">
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" color="inherit">
+						Charts Viewer
+					</Typography>
+				</Toolbar>
 			</div>
 
-			<div
-				style={{
-
-					position: "absolute",
-					top: headerHeight,
-					left: 0,
-					width: menuWidth,
-					bottom: 0,
-
-					overflowX: "hidden",
-					overflowY: "auto",
-
-					borderRight: "1px solid darkgray"
-
-				}}
-			>
+			<div className={classes.treeDiv}>
 				<FilesTree 
 					fileList={this.state.fileList}
 					setSelectedNode={this.setSelectedNode}
@@ -213,20 +183,7 @@ class App extends React.Component {
 			</div>
 
 
-			<div
-				style={{
-
-					position: "absolute",
-					top: headerHeight,
-					left: menuWidth+5,
-					right: 0,
-					bottom: 0,
-
-					//overflowX: "auto",
-					//overflowY: "auto"					
-
-				}}
-			>
+			<div className={classes.viewerDiv}>
 				<Viewer filePath={this.state.filePath}/>
 			</div>
 
@@ -236,51 +193,6 @@ class App extends React.Component {
 
 	}
 
-	_render(){
-
-		// Classi per gli stili custom
-		const {classes} = this.props;
-
-		return (
-
-			<div className={classes.root}>
-
-				<CssBaseline />
-
-				<AppBar position="fixed" className={classes.appBar}>
-					<Toolbar variant="dense">
-						<Typography variant="h6" noWrap>
-							Charts Viewer
-						</Typography>
-					</Toolbar>
-				</AppBar>
-
-				<Drawer
-					className={classes.drawer}
-					variant="permanent"
-					anchor="left"
-					classes={{
-						paper: classes.drawerPaper,
-					}}				
-				>
-					<Toolbar variant="dense" />
-					ciao
-					<FilesTree 
-						fileList={this.state.fileList}
-						setSelectedNode={this.setSelectedNode}
-					/>
-				</Drawer>
-
-				<main className={classes.content}>
-					<Toolbar variant="dense" />
-					<Button color="primary">Hello World</Button>
-					<Viewer filePath={this.state.filePath}/>
-				</main>
-			
-			</div>
-
-		);
-	}
 }
 
 export default withStyles(styles)(App);
