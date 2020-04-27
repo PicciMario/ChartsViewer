46f3e42b7bb298ed3545f7687a6f224b26d53476
Mario Piccinelli <m.piccinelli@kline.it>
2020-04-27 15:20:12 +0200

    spostato dev su porta 3006

--- public/electron.js
+++ public/electron.js
@@ -11,7 +11,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680,         webPreferences: {
     nodeIntegration: true
 }});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.loadURL(isDev ? 'http://localhost:3006' : `file://${path.join(__dirname, '../build/index.html')}`);
   if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
